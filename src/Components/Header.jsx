2d2b0d1f6e6458ec574de76533ad8a4274f49e31
atulit <coderatulit@gmail.com>
2021-06-23 03:18:37 +0530

    added link to main title

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,11 +1,14 @@
 import React from "react";
+import { Link } from "react-router-dom";
 class Header extends React.Component {
   render() {
     return (
       /* jslint ignore: start */
       <>
         <header className="bg-gray-600 text-white md:p-4 p-2 text-6xl">
-          <h1 className="">Javascript Docs</h1>
+          <h1 className="">
+            <Link to="/">Javascript Docs</Link>
+          </h1>
         </header>
       </>
       /* jslint ignore: end */
